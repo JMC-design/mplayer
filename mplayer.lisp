7bfb5911c4d62539d96f0559865904113cc016d5
Johannes Martinez <spamreceiver23@gmail.com>
2017-01-14 16:23:12 -0500

    deleted non-working code

--- mplayer.lisp
+++ mplayer.lisp
@@ -28,32 +28,8 @@
    (code :initarg :code :reader code)
    (pid :initarg :pid :reader pid)))
 
-(defun print-status (condition)
-  "Prints out pid, status, and exit code from a condition"
-  (incf *fail*)
-  (format nil "Status: ~a Pid: ~a Exit-code: ~a" (status condition) (pid condition) (code condition)))
 
 
-(defun signal-status-changes (process)
-  "signals a condition based on how a process exited"
-  (let ((status (sb-ext:process-status process))
-	(code (sb-ext:process-exit-code process))
-	(pid (sb-ext:process-pid process)))
-    (format t "shits going down!")
-    (signal  'process-status-change :status status :code code :pid pid)))
-
-
-(defun playv1 (file &key window (options *mplayer-defaults*) (status-hook #'print-status))
-  "Plays file with mplayer. You can specify an X11 window to be parented to, or options in addition to a status hook besides the basic default one."
-  (let ((arglist options))
-    (when window (setf arglist (concatenate 'list arglist `("-wid" ,(princ-to-string (xlib:window-id window))))))
-    (let* ((mp-process (sb-ext:run-program "/usr/bin/mplayer"
-					   `(,file ,@arglist)
-					   :wait nil :input :stream :output :stream :status-hook status-hook))
-	   (status (process-status mp-process)))    
-      (if (eq status :running)
-	  mp-process
-	  (signal :video-play-problem :file file :status status)))))
 
 (defun play (file &key window (options *mplayer-defaults*) (status-hook #'signal-status-changes))
   "Plays file with mplayer. You can specify an X11 window to be parented to, or options in addition to a status hook besides the basic default one."
